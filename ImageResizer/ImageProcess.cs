e966a361f03f3488930abe07739a18b228fbd502
rayainman <sihruei.he@gmail.com>
2020-03-28 20:58:01 +0800

    改名稱

--- ImageResizer/ImageProcess.cs
+++ ImageResizer/ImageProcess.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ImageResizer
 {
     public class ImageProcess
     {
+        readonly string destPath;
+        readonly string sourcePath;
+        List<string> allFiles;
+        
+        public ImageProcess(string sourcePath,string destPath)
+        {
+            this.sourcePath = sourcePath;
+            this.destPath = destPath;            
+        }
+        public async Task SetFiles()
+        {
+            allFiles =  FindImages();
+        }
+        private List<string> FindImages()
+        {
+            List<string> files = new List<string>();
+            files.AddRange(Directory.GetFiles(sourcePath, "*.png", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles(sourcePath, "*.jpg", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles(sourcePath, "*.jpeg", SearchOption.AllDirectories));
+            return files;
+        }
         /// <summary>
         /// 清空目的目錄下的所有檔案與目錄
         /// </summary>
         /// <param name="destPath">目錄路徑</param>
-        public void Clean(string destPath)
+        public void Clean()
         {
             if (!Directory.Exists(destPath))
             {
@@ -35,27 +61,37 @@ namespace ImageResizer
         /// <param name="sourcePath">圖片來源目錄路徑</param>
         /// <param name="destPath">產生圖片目的目錄路徑</param>
         /// <param name="scale">縮放比例</param>
-        public void ResizeImages(string sourcePath, string destPath, double scale)
-        {
-            var allFiles = FindImages(sourcePath);
+        public void ResizeImages(double scale)
+        {        
             foreach (var filePath in allFiles)
             {
-                Image imgPhoto = Image.FromFile(filePath);
+                //Image imgPhoto = Image.FromFile(filePath);
                 string imgName = Path.GetFileNameWithoutExtension(filePath);
-
-                int sourceWidth = imgPhoto.Width;
-                int sourceHeight = imgPhoto.Height;
-
-                int destionatonWidth = (int)(sourceWidth * scale);
-                int destionatonHeight = (int)(sourceHeight * scale);
-
-                Bitmap processedImage = processBitmap((Bitmap)imgPhoto,
-                    sourceWidth, sourceHeight,
-                    destionatonWidth, destionatonHeight);
-
+                Bitmap processedImage = ProcessBitmap(filePath, scale);
                 string destFile = Path.Combine(destPath, imgName + ".jpg");
                 processedImage.Save(destFile, ImageFormat.Jpeg);
             }
+        }
+        public async Task ResizeImagesAsync(double scale)
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var filePath in allFiles)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    string imgName = Path.GetFileNameWithoutExtension(filePath);
+                    string destFile = Path.Combine(destPath, imgName + ".jpg");
+
+                    var processedImg = ProcessBitmapAsync(filePath, scale);
+
+                    await processedImg.ContinueWith(t =>
+                    {
+                        t.Result.Save(destFile, ImageFormat.Jpeg);
+                    });
+                }));
+            }
+            await Task.WhenAll(tasks);   
         }
 
         /// <summary>
@@ -63,13 +99,13 @@ namespace ImageResizer
         /// </summary>
         /// <param name="srcPath">圖片來源目錄路徑</param>
         /// <returns></returns>
-        public List<string> FindImages(string srcPath)
+        private async Task<List<string>> FindImagesAsync()
         {
             List<string> files = new List<string>();
-            files.AddRange(Directory.GetFiles(srcPath, "*.png", SearchOption.AllDirectories));
-            files.AddRange(Directory.GetFiles(srcPath, "*.jpg", SearchOption.AllDirectories));
-            files.AddRange(Directory.GetFiles(srcPath, "*.jpeg", SearchOption.AllDirectories));
-            return files;
+            files.AddRange(Directory.GetFiles(sourcePath, "*.png", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles(sourcePath, "*.jpg", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles(sourcePath, "*.jpeg", SearchOption.AllDirectories));
+            return await Task.FromResult(files);
         }
 
         /// <summary>
@@ -81,18 +117,76 @@ namespace ImageResizer
         /// <param name="newWidth">新圖片的寬度</param>
         /// <param name="newHeight">新圖片的高度</param>
         /// <returns></returns>
-        Bitmap processBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
+        private Bitmap ProcessBitmap(string filePath, double scale)
         {
-            Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
+            Image img = Image.FromFile(filePath);
+            int destionatonWidth = (int)(img.Width * scale);
+            int destionatonHeight = (int)(img.Height * scale);
+            
+            Bitmap resizedbitmap = new Bitmap(destionatonWidth, destionatonHeight);
             Graphics g = Graphics.FromImage(resizedbitmap);
             g.InterpolationMode = InterpolationMode.High;
             g.SmoothingMode = SmoothingMode.HighQuality;
             g.Clear(Color.Transparent);
             g.DrawImage(img,
-                new Rectangle(0, 0, newWidth, newHeight),
-                new Rectangle(0, 0, srcWidth, srcHeight),
+                new Rectangle(0, 0, destionatonWidth, destionatonHeight),
+                new Rectangle(0, 0, img.Width, img.Height),
                 GraphicsUnit.Pixel);
             return resizedbitmap;
         }
+
+        private async Task<Bitmap> ProcessBitmapAsync(string filePath, double scale)
+        {
+            return await Task.Run(async () =>
+            {
+                Image img = Image.FromFile(filePath);
+                int destionatonWidth = (int)(img.Width * scale);
+                int destionatonHeight = (int)(img.Height * scale);
+                Bitmap resizedbitmap = new Bitmap(destionatonWidth, destionatonHeight);
+
+            
+                Graphics g = Graphics.FromImage(resizedbitmap);
+                g.InterpolationMode = InterpolationMode.High;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                await Task.Run(async () => { 
+                    g.Clear(Color.Transparent);
+                    g.DrawImage(img,
+                    new Rectangle(0, 0, destionatonWidth, destionatonHeight),
+                    new Rectangle(0, 0, img.Width, img.Height),
+                    GraphicsUnit.Pixel);
+                });
+            return resizedbitmap;
+            });     
+        }
+
+        private void Log(string functionName)
+        {
+            Console.WriteLine(functionName+ $"{ DateTime.Now.ToString("hh:MM:ss-fff")}]");
+            //Console.WriteLine(functionName +　$"Thread:{Thread.CurrentThread.ManagedThreadId}]");
+        }
+
     }
 }
+
+
+//   public async Task ResizeImagesAsync(double scale) 另外的寫法
+/*
+foreach (var filePath in allFiles)
+{
+
+    tasks.Add(Task.Run(async () =>
+    {
+        Console.WriteLine("1");
+        string imgName = Path.GetFileNameWithoutExtension(filePath);
+        string destFile = Path.Combine(destPath, imgName + ".jpg");
+
+
+        Console.WriteLine("2");
+        var processedImg = await ProcessBitmapAsync(filePath, scale);
+        Console.WriteLine("3");
+        processedImg.Save(destFile, ImageFormat.Jpeg);
+
+    }));     
+}
+Task.WhenAll(tasks).Wait();
+*/
--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,21 +10,49 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            ImageProcess imageProcess = new ImageProcess(sourcePath,destinationPath);
 
-            ImageProcess imageProcess = new ImageProcess();
+            Stopwatch oldStyle = new Stopwatch();
+            Stopwatch newStyle = new Stopwatch();
+            var oldStyletime = new long();
+            var newStyletime = new long();
 
-            imageProcess.Clean(destinationPath);
+            for (int i =0; i < 1; i++)
+            {
+                Console.WriteLine($"同步開始---");
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
-            sw.Stop();
+                imageProcess.Clean();
+                await imageProcess.SetFiles();
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+                oldStyle.Start();
+                imageProcess.ResizeImages(2.0);
+                oldStyle.Stop();
+                oldStyletime += oldStyle.ElapsedMilliseconds;
+                Console.WriteLine($"同步結束");
+
+                Console.WriteLine($"非同步開始");
+
+                imageProcess.Clean();
+                await imageProcess.SetFiles();
+
+                newStyle.Start(); 
+                await imageProcess.ResizeImagesAsync(2.0);           
+                newStyle.Stop();
+
+                Console.WriteLine($"非同步結束");
+                newStyletime += newStyle.ElapsedMilliseconds;
+            }
+
+            Console.WriteLine($"非同步花費時間: {newStyle.ElapsedMilliseconds} ms");
+            Console.WriteLine($"同步花費時間: {oldStyle.ElapsedMilliseconds} ms");
+            Console.WriteLine(string.Format("效能提升比例：{0:0.000}", ((double)oldStyletime - newStyletime) / oldStyletime));
+
+
+            Console.ReadKey();
         }
     }
 }
